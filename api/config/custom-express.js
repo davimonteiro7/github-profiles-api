2724905a676e2223c2f816f73910328d0f3da04b
davimonteiro7 <davi.monteiro7@gmail.com>
2020-10-22 17:16:31 -0300

    feat/refactor: views show all required and validate username, whithout css for now.

--- api/config/custom-express.js
+++ api/config/custom-express.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const consign = require('consign');
 const path = require('path');
-const hbs = require('hbs');
-
 
 module.exports = () => {
     const app = express();
     
-    
-
-
     app.use('/public',express.static(path.resolve('./views/public')))
         .use(express.urlencoded({extended: true}))
         .set('views', path.resolve('./views'))
-        .set('view engine', 'hbs');
-    
-    hbs.registerPartials(path.resolve('./views/partials'));
-    
+        .set('view engine', 'hbs');    
     consign()
         .include('controllers')
         .into(app);
--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,23 +2,17 @@ const userClient = require('../services/userClients')();
 const UserModel = require('../models/userModel');
 
 module.exports = (app) => {
+
     app.get('/',(req, res) => {
-
-
-        res.render('index');
+        res.render('index', {data: false});
     })
     
-    app.post('/user', (req, res) => {
-        const username  = req.body.username;
-        console.log(username);
+    app.get('/profile', (req, res) => {
+        const username  = req.query.username;
         const userModel = new UserModel(userClient, username);
         
         userModel.getUser.then(async user => {
-            if(!user.name){
-                console.log('User not found!');
-                res.send('User not found!')
-            }
-
+            
             var userWithRepos = {
                 username: user.login,
                 avatar: user.avatar_url,
@@ -29,16 +23,19 @@ module.exports = (app) => {
                     return repos.map(repo => ({
                         stars:repo.stargazers_count,
                         forks:repo.forks,
-                        repo_name:repo.name 
+                        repo_name:repo.name
                     })).sort((a, b) => {
                         return b.forks - a.forks || b.stars - a.stars;
                     }).splice(0,4);
                 })
             }
             console.log(userWithRepos);
-            res.redirect('/')
+            res.render('index', {data: userWithRepos});
         }).catch(err => {
-            res.json(err);
+            console.log('This username is not valid!');
+            res.render('index', {data: {
+                validation: 'This username is not valid!'
+            }});
         });
     });
 }
